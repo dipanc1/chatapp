2ba028dca90dc827290021c34644fee0716087cc
dipanc1 <dipanchhabra@gmail.com>
2022-04-07 09:47:38 +0530

    before group chat feature

--- backend/Router/users.js
+++ backend/Router/users.js
@@ -11,7 +11,6 @@ router.post("/register", async(req, res) => {
         const salt = await bcrypt.genSalt(10)
         const hashedPassword = await bcrypt.hash(req.body.password, salt)
 
-
         //create new user
         const newUser = new User({
             username: req.body.username,
