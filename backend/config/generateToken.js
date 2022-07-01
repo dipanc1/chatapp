7fb98187f96940cc169826fe722d80c72ca1df83
Dipan Chhabra <73923779+dipanc1@users.noreply.github.com>
2022-07-01 14:40:22 +0530

    Delete backend/config directory

--- backend/config/generateToken.js
+++
@@ -1,9 +0,0 @@
-const jwt = require("jsonwebtoken");
-
-const generateToken = (id) => {
-    return jwt.sign({ id }, "jatt", {
-        expiresIn: "30y",
-    });
-};
-
-module.exports = generateToken;
--- backend/config/mongo_auth.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    mongo_url: 'mongodb+srv://chatapp:chatapp@cluster0.hrwqz.mongodb.net/ChatApp?retryWrites=true&w=majority'
-}
