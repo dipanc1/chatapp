2f8d572d6b0926443ed09d1770dcfbe488084e2f
dipanc1 <dipanchhabra@gmail.com>
2023-09-03 09:43:26 +0530

    updated fiel name

---
+++ frontend/src/context/SocketContext.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { io } from "socket.io-client";
+import { backend_url } from "../utils";
+
+const ENDPOINT = backend_url;
+
+var socket = io(ENDPOINT);
+
+export const SocketContext = React.createContext(socket);
+
+export const SocketContextProvider = ({ children }) => {
+
+    return (
+        <SocketContext.Provider value={socket}>
+            {children}
+        </SocketContext.Provider>
+    );
+}
