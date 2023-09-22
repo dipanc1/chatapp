c6716220bc038926bd529849706a99a2aef2571f
dipanc1 <dipanchhabra@gmail.com>
2023-09-22 21:15:34 +0530

    updating download funauitonalty

--- phoneApp/components/Miscellaneous/Message.js
+++ phoneApp/components/Miscellaneous/Message.js
@@ -1,58 +1,37 @@
-import { Box, Button, Center, Flex, HStack, Icon, IconButton, Modal, Text } from 'native-base'
+import { Box, Flex, HStack, IconButton, Text } from 'native-base'
 import React from 'react'
 import { PhoneAppContext } from '../../context/PhoneAppContext';
 import { format } from 'timeago.js';
 import axios from 'axios';
 import { backend_url, checkFileExtension } from '../../utils';
-import { Image, TouchableOpacity } from 'react-native';
+import { Image, Linking, TouchableOpacity } from 'react-native';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
 import { AUDIO, DOC, IMAGE, PDF, PPT, TXT, VIDEO, XLS } from '../../constants';
 
 const size = 32;
+
+const downloadFile = async (content) => {
+    try {
+        Linking.openURL(content);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 const DownloadFileComponent = ({ content, children }) => {
     return (
         <Flex direction={'row'} alignItems={'center'} width={'100%'} justifyContent={'space-around'} position={'relative'}>
             {children}
-            <IconButton icon={<MaterialIcons name="file-download" size={size} color={'white'} />} />
+            <IconButton icon={<MaterialIcons name="file-download" size={size} color={'white'} />} onPress={
+                () => downloadFile(content)
+            } />
         </Flex>
     )
 }
 
-const ImageViewer = ({ showModal, setShowModal, content }) => {
-    return <Center>
-        <Modal isOpen={showModal} onClose={() => setShowModal(false)}>
-            <Modal.Content>
-                <Modal.CloseButton />
-                <Modal.Header>Image</Modal.Header>
-                <Modal.Body display={'flex'} justifyContent={'center'} alignItems={'center'}>
-                    {checkFileExtension(content) === IMAGE && <Image source={
-                        {
-                            uri: content
-                        }
-                    } alt={'image'} style={{
-                        width: '100%',
-                        height: '100%'
-                    }} />}
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button.Group space={2}>
-                        <Button variant={'outline'} colorScheme="violet" rounded={'lg'} onPress={() => setShowModal(false)}>
-                            Cancel
-                        </Button>
-                        <Button rounded={'lg'} bg={'primary.300'}>
-                            {'Download'}
-                        </Button>
-                    </Button.Group>
-                </Modal.Footer>
-            </Modal.Content>
-        </Modal>
-    </Center>;
-};
 
 const Message = ({ messages, own, sameSender, sameTime, user }) => {
     const { selectedChat } = React.useContext(PhoneAppContext);
-    const [showModal, setShowModal] = React.useState(false);
-
 
     React.useEffect(() => {
         const readLastMessage = async () => {
@@ -99,9 +78,8 @@ const Message = ({ messages, own, sameSender, sameTime, user }) => {
                     <Box rounded={'lg'} p={'2'} marginRight={own ? '10' : '10'} marginLeft={own ? '10' : '0'} bg={own ? 'primary.400' : 'primary.200'}>
                         {checkFileExtension(messages.content) === IMAGE ?
                             <Box position={'relative'} h='300px'>
-                                <IconButton position={'absolute'} top={'1'} right={'1'} icon={<MaterialIcons name="file-download" size={size} color={'white'} />} />
                                 <TouchableOpacity onPress={
-                                    () => setShowModal(true)
+                                    () => downloadFile(messages.content)
                                 }>
                                     <Image
                                         style={{
@@ -115,7 +93,6 @@ const Message = ({ messages, own, sameSender, sameTime, user }) => {
                                             }
                                         } alt={'image'} />
                                 </TouchableOpacity>
-                                <ImageViewer showModal={showModal} setShowModal={setShowModal} content={messages.content} />
                             </Box>
                             : checkFileExtension(messages.content) ?
                                 <Box display='flex' alignItems='center' justifyContent='center' flexDirection='column'>
